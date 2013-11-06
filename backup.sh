48923c020f55b1cac8f18fac90c0ddc55dd0fc58
Mihkel Baranov <mihkel@resac.eu>
2013-11-06 15:52:27 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 clear
 echo 'Starting, this might take some time.'
-
-#Making simple tar.gz backup file
+#Making simple tar.gz backup from /home/sites/ folder(s)
 tar -czPf backup.tar.gz /home/sites/
 echo 'Backup done!'
 
