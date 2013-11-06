347e3ca5296a89e031eb2c4f53309f9c0bcf2f70
Mihkel Baranov <mihkel@resac.eu>
2013-11-06 16:04:27 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,7 +2,15 @@
 clear
 echo 'Starting, this might take some time.'
 #Making simple tar.gz backup from /home/sites/ folder(s)
-tar -czPf backup.tgz /home/sites/
+tar -czPf backup.tar.gz /home/sites/
 echo 'Backup done!'
 
+echo 'Starting upload to ftp server'
 #Todo, add some upload functions
+ftp -n ftp.resac.eu <<END_SCRIPT
+quote USER backup
+quote PASS xxxXXxx
+put backup.tar.gz
+quit
+END_SCRIPT
+
