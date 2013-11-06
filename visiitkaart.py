05d70a2360034d0bc82692ab33c91bd65ddf0723
Mihkel Baranov <mihkel@resac.eu>
2013-11-06 09:27:29 +0200

    Update visiitkaart.py

--- visiitkaart.py
+++ visiitkaart.py
@@ -1,1 +1,17 @@
-fuu
+
+name = raw_input('Enter your name: ');
+name = name.capitalize()
+occupation = raw_input('Enter your occupation: ');
+email = raw_input('Enter your email: ')
+text = "Visiitkaart"
+empty = ""
+print "|", text.center(50, "="), '|'
+#Empty line
+print "|", empty.center(50, " "), '|'
+print "|", name.center(50), "|"
+print "|", occupation.center(50), "|"    
+print "|", email.center(50), "|"
+#Empty line
+print "|", empty.center(50, " "), '|'
+print "|", empty.center(50, "="), '| \n'
+
