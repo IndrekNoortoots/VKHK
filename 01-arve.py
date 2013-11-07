df211b2eb6565f327bce6a0fc1223f47d690aed4
Mihkel Baranov <mihkel@resac.eu>
2013-11-07 12:37:32 +0200

    Create 01-arve.py

---
+++ 01-arve.py
@@ -0,0 +1,30 @@
+# coding: latin-1
+
+print '{:^75}'.format('Arve')
+print
+head = '{:<30} {:<20} {:<20} {:>10}'
+head_2 = '{:<29} {:<21} {:<21} {:>10}'
+head_3 = '{:<29} {:<10}'
+body = '{:<19}{:>15}{:>15}{:>15}'
+price = '{:>49}{:>15}'
+prce = '{:>50}{:>15}'
+footer = '{:<29}{:<15}'
+print head.format('Arve väljastaja', 'Arve saaja','Arve number:','4121')
+print head_2.format('Villu Pomm', 'Peeter Pesupäev','Kuupäev:','07.11.2013')
+print head_3.format('Villu tn','Karjääri tn')
+for x in xrange(1,5):
+    print
+    pass
+print body.format('Kaup','Hind','Kogus','Kokku')
+print body.format('Hapukapsas','5 KG','1','5')
+print body.format('Kartul','5','1 KG','5')
+print
+print price.format('Vahesumma', '10')
+print prce.format('Käibemaks 20%', '10,6')
+print price.format('Kokku', '20,60')
+for x in xrange(1,2):
+    print
+    pass
+print footer.format('Kontaktid','Arveldus')
+print footer.format('villu@gmail.com','Swedbank')
+print footer.format('telefon:1135135 ','IBN 14535135135135')
