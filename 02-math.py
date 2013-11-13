9beada27f6d8c3b2ce2979c3eedc063bff681a4e
Mihkel Baranov <mihkel@resac.eu>
2013-11-13 10:45:49 +0200

    Update 02-math.py

--- 02-math.py
+++ 02-math.py
@@ -1,15 +1,1 @@
-# coding: latin-1
-import math
-print "#1 Mingi kolmnurk, sin(20kraadi), y külg 20m ja x külg vaja leida "
-print
-
-x = 65/math.sin(20 * math.pi/180)
-print 'vastus: x = ',round(x,2),'m';
-
-
-
-num = raw_input("#2 Sisesta mingi number mis arvutatakse cos, tan, sin = ")
-s = float(num)
-print "Sin: ",math.sin(s * math.pi/180)
-print "Cos: ",math.cos(s * math.pi/180)
-print "Tan: ",math.tan(s * math.pi/180)
+ 
