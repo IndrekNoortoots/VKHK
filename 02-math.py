e6483482e6fe1e6c9b44eb502ecd2d79fea176ce
Mihkel Baranov <mihkel@resac.eu>
2013-11-13 10:41:38 +0200

    Update 02-math.py

--- 02-math.py
+++ 02-math.py
@@ -9,7 +9,7 @@ print 'vastus: x = ',round(x,2),'m';
 
 
 num = raw_input("#2 Sisesta mingi number mis arvutatakse cos, tan, sin = ")
-s = int(num)
+s = float(num)
 print "Sin: ",math.sin(s * math.pi/180)
 print "Cos: ",math.cos(s * math.pi/180)
 print "Tan: ",math.tan(s * math.pi/180)
